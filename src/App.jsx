e81c3ba7a387c92d41d03f6b80ffa5b0b1cbc259
tanviralways <parthotanvir1234@gmail.com>
2023-12-08 20:56:58 +0600

    removde file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import ProductList from "./components/ProductList/ProductList";
-import Books from "./components/books/books";
-import Counter from "./components/Counter/Counter";
+
 
 export const App = () => {
   return (
     <div>
       <ProductList />
-      {/* <Counter />    */}
-      {/* <Books /> */}
     </div>
   );
 };
